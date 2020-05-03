dfa6ea098b31ea8b5d9e5bdfc18f11cd5f1a7d74
Aaron <aaronclayton94@hotmail.com>
2020-05-03 09:38:45 +0800

    add html + css and js dependencies

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Dependencies
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3000;
